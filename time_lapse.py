cb67dd82ceb79a16390fd2fc268f31e2135d4428
Nobuo Tsukamoto <nobuo@localhost.localdomain>
2016-08-11 20:52:16 +0900

    chamge interval

--- time_lapse.py
+++ time_lapse.py
@@ -4,6 +4,8 @@
 import time
 import datetime
 import picamera
+import time
+import sys
 
 def cameraCapture():
 
@@ -11,13 +13,31 @@ def cameraCapture():
     now_string = now.strftime("%Y%m%d_%H%M%S")
 
     camera = picamera.PiCamera()
-    camera.resolution = (2592, 1944)
+    camera.resolution = (1920, 1080)
     camera.start_preview()
 
     time.sleep(5)
     camera.capture("/home/pi/" + now_string + "_image.jpg")
+    
+    camera.stop_preview()
+    camera.close()
 
 if __name__ == '__main__':
-    # test
-    cameraCapture()
+
+    capture_count = 0
+    argvs = sys.argv
+    argc = len(argvs)
+
+    if (argc > 0):
+        capture_count = int(argvs[1])
+    else:
+        capture_count = 8 * 60 * 6    # 8 hours
+
+    print("caputre count : " + str(capture_count))
+    
+    for num in range(0, capture_count):
+        print("num of capture : " + str(num + 1))
+        cameraCapture()
+        time.sleep(4)
+        
 
